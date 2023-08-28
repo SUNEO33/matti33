e6d065b16e84648a0ba24d492e388461a882225c
SUNEO33 <rikuto-kawata-k23f@stu.kbc.ac.jp>
2023-08-28 14:02:01 +0900

    tff
    iii

--- matti33/matti33/GameMainScene.cpp
+++ matti33/matti33/GameMainScene.cpp
@@ -93,8 +93,8 @@ int GameMainScene_Initialize(void)
 	if (GameCount == 0)
 	{
 		GameScore = 0;       //スコアの初期化
-		GameLevel = 1;       //ゲームレベルの初期化
-		Set_StageMission(3);    //ミッションの初期化
+		GameLevel = 0;       //ゲームレベルの初期化
+		Set_StageMission(0);    //ミッションの初期化
 		GameCount++;            //次回の設定
 	}
 	else
